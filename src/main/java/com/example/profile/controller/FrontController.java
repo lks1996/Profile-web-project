5f9bfb254de8c7838546029d7b4060d49f290020
lks1996 <mosplee@naver.com>
2025-12-24 22:35:01 +0900

    로깅추가

--- src/main/java/com/example/profile/controller/FrontController.java
+++ src/main/java/com/example/profile/controller/FrontController.java
@@ -3,10 +3,12 @@ package com.example.profile.controller;
 import com.example.profile.dto.ProfileWrapper;
 import com.example.profile.service.FrontProfileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class FrontController {
@@ -17,6 +19,7 @@ public class FrontController {
     public String index(Model model) {
         // 1. 화면에 보여줄 데이터 조회 (숨김 처리된 것 제외됨)
         ProfileWrapper profile = frontProfileService.getPublicProfile();
+        log.info("Somebody got profile");
 
         // 2. 모델에 담기
         model.addAttribute("profile", profile);
